0682df855d6bc6c13bea9968749d48f5e22bc65e
wwndbs <wwndbs23@gmail.com>
2022-09-20 00:41:17 +0900

    Feat : 메일 중요/읽음 여부 변경 기능, 스팸 처리 기능 추가
    - author : 정주윤

--- src/main/java/com/gd/workpp/mail/controller/AjaxMailController.java
+++ src/main/java/com/gd/workpp/mail/controller/AjaxMailController.java
@@ -2,6 +2,7 @@ package com.gd.workpp.mail.controller;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,6 +18,7 @@ import com.gd.workpp.common.model.vo.Attachment;
 import com.gd.workpp.common.template.FileUpload;
 import com.gd.workpp.mail.model.service.MailServiceImpl;
 import com.gd.workpp.mail.model.vo.Mail;
+import com.gd.workpp.member.model.vo.Member;
 
 @Controller
 public class AjaxMailController {
@@ -24,10 +26,15 @@ public class AjaxMailController {
 	@Autowired
 	private MailServiceImpl mService;
 	
+	/**
+	 * Author : 정주윤
+	 * 메일을 임시저장 하거나 이미 임시저장된 메일을 수정 처리해 주는 메소드
+	 * @return result : 성공 여부
+	 * @return mailNo : 임시저장 후 부여된 메일번호
+	 */
 	@ResponseBody
 	@RequestMapping(value="save.ma", produces="application/json; charset=UTF-8")
 	public String saveMail(Mail m, MultipartFile[] upfile, HttpSession session, Model model) {
-		System.out.println(upfile.length);
 		
 		ArrayList<Attachment> atList = new ArrayList<>();
 		int result = 0;
@@ -94,5 +101,77 @@ public class AjaxMailController {
 		return jObj.toJSONString();
 	
 	}
+	
+	/**
+	 * Author : 정주윤
+	 * 개별 메일의 중요 여부를 변경 처리해 주는 메소드
+	 * @param type : {'Y': 중요메일로 변경, 'N': 중요메일 해제}
+	 * @param no : 변경하고자 하는 메일번호
+	 */
+	@ResponseBody
+	@RequestMapping(value="updateImp.ma", produces="text/html; charset=UTF-8")
+	public String updateMailImportant(String type, int no, HttpSession session) {
+		
+		Member mem = (Member)session.getAttribute("loginUser");
+		String email = mem.getEmail();
+		
+		int result = mService.updateMailImportant(type, no, email);
+		
+		return result > 0 ? "success" : "fail";
+
+	}
+	
+	/**
+	 * Author : 정주윤
+	 * 개별 메일의 읽음 여부를 변경 처리해 주는 메소드
+	 * @param type : {'Y': 읽은메일로 변경, 'N': 안읽은메일로 변경}
+	 * @param no : 변경하고자 하는 메일번호
+	 */
+	@ResponseBody
+	@RequestMapping(value="updateRead.ma", produces="text/html; charset=UTF-8")
+	public String updateMailRead(String type, int no, HttpSession session) {
+		
+		Member mem = (Member)session.getAttribute("loginUser");
+		String email = mem.getEmail();
+		
+		int result = mService.updateMailRead(type, no, email);
+		
+		return result > 0 ? "success" : "fail";
+
+	}
+
+	/**
+	 * Author : 정주윤
+	 * 체크박스로 선택된 메일들을 읽음 처리/스팸 처리 해주는 메소드
+	 * @param type : 어떤 상태값을 변경할 건지 (읽음여부, 스팸여부)
+	 * @param checkMailNo : 선택된 메일번호들
+	 */
+	@ResponseBody
+	@RequestMapping(value="updateStatus.ma", produces="text/html; charset=UTF-8")
+	public String updateMailStatus(String type, String checkMailNo, HttpSession session) {
+
+		Member mem = (Member)session.getAttribute("loginUser");
+		String email = mem.getEmail();
+		
+		// 읽음처리, 스팸처리
+		int result1 = mService.updateMailStatus(type, checkMailNo, email);
+
+		if(type == "mail_spam") {
+			
+			// 메일번호로 보낸사람 주소 조회
+			List<String> senderList = new ArrayList<>();
+			senderList = mService.selectSender(checkMailNo); // ["user24@workpp.com", "goodee@gmail.com"]
+			System.out.println(senderList);
+			
+			// 보낸사람 주소를 스팸 목록에 추가
+			int result2 = mService.insertSpam(senderList, email);
+			
+			return result1 * result2 > 0 ? "success" : "fail";
+		}
+		
+		return result1 > 0 ? "success" : "fail";
+		
+	}
+	
 	
 }
