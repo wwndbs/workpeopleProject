ab12aeca368c6580b45acf77ce5300eadf9168e9
Youngheon Choi <102468071+eEonTown@users.noreply.github.com>
2022-09-19 10:50:33 +0900

    Update : 스타일, vo객체 수정
    - Author : 최영헌

--- src/main/java/com/gd/workpp/approval/model/vo/Approval.java
+++ src/main/java/com/gd/workpp/approval/model/vo/Approval.java
@@ -18,4 +18,7 @@ public class Approval {
 	private String status;
 	private String approvalDate;
 	private int referenceStatus;
+	private String depName;
+	private String jobName;
+	private String profImg;
 }
